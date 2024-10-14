f6bddc4b5ee2bb6b63245a43e5df9966fd332b7b
KIranMehta075 <keerang.mehta@gmail.com>
2024-10-14 21:18:07 +0530

    Pagination component and pagination added

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,43 @@
+// 1. make pagination function/ component
+// 2. need right left arrow with initial state
+// import in trending js
+// should be stick at bottom of the page
+
+import React from 'react'
+
+export const Pagination = ({handleNextPage, handlePreviousPage, currentPage}) => {
+    
+  return (
+    <div className="flex items-center bg-black text-white justify-center space-x-4 mt-4 py-4">
+        <button className="transition-transform duration-300 ease-in-out transform hover:scale-125 hover:bg-gray-500 text-white  px-4 py-2"
+        onClick = {handlePreviousPage}>
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7"></path>
+        </svg>
+        </button>
+        <span className="text-xl"> {currentPage} </span>
+        <button 
+  onClick={handleNextPage} 
+  className="transition-transform duration-300 ease-in-out transform hover:scale-125 hover:bg-gray-500 text-white  px-4 py-2"
+>
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7"></path>
+        </svg>
+        </button>
+    </div>
+  )
+}
--- src/components/TrendingMovie/TrendingMovie.jsx
+++ src/components/TrendingMovie/TrendingMovie.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Card from '../Card/Card'
 import banner from '../../Assets/Images/banner-wallpaper.jpg'
+import { Pagination } from '../Pagination/Pagination';
 
 
 const metrics = [
@@ -16,12 +17,13 @@ function TrendingMovie() {
 
   const [movies, setMovies] = useState([]);
   const [isListed, setIsListed] = useState([]);
+  const [pageno, setPageNo] = useState(1);
 
    const handleIsLikedWatchList = (id) =>{
       const newList = [...isListed];
       newList.push(id);
       setIsListed(newList);
-   }
+  }
 
    const handleRemoveFromWatchList = (id)=> {
     const newList = isListed.filter((movieId)=>{
@@ -29,10 +31,19 @@ function TrendingMovie() {
     })
     //localStorage.setItem('watchList',JSON.stringify(newWatchList));
     setIsListed(newList);
-   }
+  }
+
+  const handleNextPage = () => {
+    setPageNo(pageno + 1);
+  }
+
+  const handlePreviousPage = ( ) =>{
+    if(pageno > 1){
+    setPageNo(pageno - 1);}
+  }
 
   useEffect(() => {
-    axios.get('https://api.themoviedb.org/3/trending/movie/day?api_key=2b1dbea324598f2a439aa6f16c25bb7e').then(function(response){
+    axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=2b1dbea324598f2a439aa6f16c25bb7e&page=${pageno}`).then(function(response){
       // console.log(response);
       let movies = response.data.results;
       
@@ -42,12 +53,12 @@ function TrendingMovie() {
       console.error("Error fetching trending movies:", error);
     });
 
-  },[])
+  },[pageno]) //it will make sure to execute callback in mounting as well as on updation of pageNo state
 
     return (
         <>
         <h2 className="text-sky-500 mx-2 font-bold text-2xl text-center py-2">Trending Movies</h2>
-        <div className="flex flex-wrap justify-around bg-gray-100 gap-8">
+        <div className="flex flex-wrap justify-around bg-gray-100 gap-8 py-4">
           {movies.map((movieObj, index) => (
             <Card
               key = {movieObj.id}
@@ -62,6 +73,10 @@ function TrendingMovie() {
             />
           ))}
         </div>
+        <Pagination
+        handleNextPage = {handleNextPage}
+        handlePreviousPage = {handlePreviousPage}
+        currentPage = {pageno}/>
         </>
       );
 }
